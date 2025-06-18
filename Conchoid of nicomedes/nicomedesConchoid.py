60987af91cd21f97f34c5684a23577b877c4dba4
Arran Cheung <24ac0736@barton.ac.uk>
2025-06-18 20:57:55 +0100

    first commit

---
+++ Conchoid of nicomedes/nicomedesConchoid.py
@@ -0,0 +1,228 @@
+from manim import *
+import numpy as np
+
+class Conchoid(MovingCameraScene):
+
+    def construct(self):
+        
+        title = Tex("Conchoid of Nicomedes").move_to(UP*2.5)
+        title.font_size=60
+        self.play(Write(title))
+        self.wait(2)
+
+        ac = Tex("Arran Cheung").move_to(UP*1.5)
+        sf = Tex("Samim Fayazi").move_to(UP*1)
+        kh = Tex("Kenneth Ha").move_to(UP*0.5)
+
+        self.play(Write(VGroup(ac,sf,kh)))
+
+        self.wait(3)
+
+        self.play(FadeOut(VGroup(ac,sf,kh)))
+        self.play(FadeOut(title))
+
+        self.wait(0.3)
+
+        # What is a conchoid + History
+
+        whatIsAConchoid = Tex("What is a conchoid?").move_to(UP*1.5)
+        self.play(Write(whatIsAConchoid))
+
+        conchoidDescription = Tex("A conchoid is a curve generated using a fixed point, a straight line and a length k")
+        conchoidDescription.font_size = 34
+        self.play(Write(conchoidDescription))
+        self.wait(3)
+
+        inventor = Tex("Invented by Greek mathematician Nicomedes").move_to(DOWN*1.5)
+        inventor.font_size = 34
+        self.play(Write(inventor))
+        self.wait(3.4)
+
+        # end of history section
+
+        self.play(FadeOut(VGroup(whatIsAConchoid, conchoidDescription, inventor)))
+
+        # Practical uses
+
+        usesOfCN = Tex("Applications").move_to(UP*1.5)
+        self.play(Write(usesOfCN))
+
+        angleTrisection = Tex("Angle trisection: Splitting an angle into 3 equal angles").move_to(UP*0.5)
+        angleTrisection.font_size = 34
+        self.play(Write(angleTrisection))
+
+        LineonePointfive = Line(start=LEFT*2 +DOWN*2.5 ,end= DOWN*2.5)
+        LineonePointfive.color = YELLOW
+        self.play(Write(LineonePointfive))
+
+        verticalLine = Line(start= LEFT*2+DOWN*2.5, end=DOWN*0.5+LEFT*2)
+        verticalLine.color = YELLOW
+        self.play(Write(verticalLine))
+
+        diagLine1 = Line(start=LEFT*2+DOWN*2.5, end=LEFT*0.55+DOWN*0.5)
+        diagLine2 = Line(start=LEFT*2+DOWN*2.5, end=DOWN*1.5)
+
+        self.play(Write(diagLine1))
+        self.play(Write(diagLine2))
+
+        # # Basic construction
+
+        # originDot = Dot(ORIGIN, color=RED)
+        # self.play(Write(originDot))
+        # originLabel = Tex("O").move_to(RIGHT*0.5 + DOWN*0.5)
+        # self.play(Write(originLabel))
+        # self.wait(0.3)
+
+        # line = Line(start=4*LEFT+3*DOWN, end=4 * RIGHT + 3 * UP)
+        # lineLabel = Tex("m").move_to(4.5*LEFT+3*DOWN)
+        # self.play(Write(line))
+        # self.play(Write(lineLabel))
+        # self.wait(0.3)
+
+        # straightLine = Line(start=4*LEFT + 1*UP, end=4*RIGHT+1*UP)
+        # self.play(Write(straightLine))
+        # straightLineLabel = Tex("l").move_to(4.5*LEFT + 1*UP)
+        # self.play(Write(straightLineLabel))
+        # self.wait(0.3)
+
+        # lineMLabel = Tex("m is a line passing through O and line l").move_to(1*RIGHT+3*DOWN)
+        # lineMLabel.font_size = 34
+        # self.play(Write(lineMLabel))
+        # self.wait(0.6)
+
+        # self.play(FadeOut(lineMLabel))
+
+        # p1, p2 = line.get_start(), line.get_end()
+        # p3, p4 = straightLine.get_start(), straightLine.get_end()
+        # intersection = line_intersection((p1,p2), (p3,p4))
+        # intersectionDot = Dot(intersection, color=RED)
+        # self.add(intersectionDot)
+        # intersectionLabel = Tex("p").move_to(intersection + DOWN*0.5 + RIGHT*0.5)
+        # self.play(Write(intersectionLabel))
+
+        # k = 3
+        # gradient = 3/4
+        # angle = np.arctan(gradient)
+        # xComp = k*np.cos(angle)
+        # yComp = k*np.sin(angle)
+
+        # Q1 = Dot(intersection + xComp * LEFT + yComp * DOWN, color=RED)
+        # self.play(Write(Q1))
+        # q1Label = Tex("Q1").move_to(intersection + xComp * LEFT + yComp * DOWN + 0.5 *RIGHT + 0.5 * DOWN)
+        # self.play(Write(q1Label))
+
+        # Q2 = Dot(intersection + xComp * RIGHT + yComp * UP, color=RED)
+        # self.play(Write(Q2))
+        # q2Label = Tex("Q2").move_to(intersection + xComp * RIGHT + yComp * UP + 0.5 * RIGHT + 0.5 * DOWN)
+        # self.play(Write(q2Label))
+
+        # distanceKText = Tex("distance[Q1,p] = distance[Q2,p] = k").move_to(LEFT*2 + 2.5*UP)
+        # distanceKText.font_size=34
+        # self.play(Write(distanceKText))
+        # self.wait(1.2)
+
+        # self.play(FadeOut(distanceKText))
+
+        # formationOfConchoid = Tex("The locus of Q1 and Q2 as p varies along l is the").move_to(LEFT*2.7 + 2.5*UP)
+        # conchoidOfNicomedes = Tex("Conchoid of Nicomedes").move_to(LEFT*0.75 + 2*UP)
+        # formationOfConchoid.font_size = 34
+        # conchoidOfNicomedes.font_size = 34
+        # self.play(Write((VGroup(formationOfConchoid, conchoidOfNicomedes))))
+
+        # # end of construction
+
+        # self.wait(4)
+
+        # # transition
+
+        # self.play(FadeOut(VGroup(originDot, originLabel, line, lineLabel, straightLine, straightLineLabel, intersectionDot, intersectionLabel, formationOfConchoid, conchoidOfNicomedes, Q1, Q2, q1Label, q2Label)))
+
+        # # equations
+
+        # equations = Tex("Equations").move_to(UP*2.5)
+        # self.play(Write(equations))
+
+        # conchoidEq = Tex(r"$r=\frac{b}{sin\theta}\pm k$").move_to(UP*1)
+        # self.play(Write(conchoidEq))
+
+        # highlight = Rectangle().move_to(UP*1)
+        # self.play(Circumscribe(highlight, color=YELLOW))
+
+
+        
+        # #explainationB = Tex("Where the line y=b is the asymptote")
+        # #explainationB.font_size = 34
+        # #self.play(Write(explainationB))
+
+        # #explainationK = Tex("And k is a given distance").move_to(DOWN*1)
+        # #explainationK.font_size = 34
+        # #self.play(Write(explainationK))
+
+        # # end of equations
+
+        # self.wait(3)
+        
+        # # transition
+
+        # self.play(FadeOut(VGroup(equations, conchoidEq, explainationB, explainationK)))
+
+        # # start of overall construction
+
+        # curves = VGroup()
+        
+        # def ConchoidConstruct(l):
+        #     a = 1
+        #     tMax = ValueTracker(0.01)
+
+        #     lvalue = Tex("k = " , -l).move_to(DOWN * 3 + 3*LEFT)
+        #     self.play(FadeIn(lvalue))
+
+        #     def conchoid_func(t):
+        #         eps = 1e-3
+
+        #         sin_t = np.sin(t)
+        #         if abs(sin_t) < eps:
+        #             sin_t = eps if sin_t >= 0 else -eps
+
+        #         r = a / sin_t + l # polar version of conchoid
+        #         return np.array([r * np.cos(t), r * np.sin(t), 0]) # converting coords into cartesian
+            
+        #     def conchoid_func_minus(t):
+        #         eps = 1e-3
+
+        #         sin_t = np.sin(t)
+        #         if abs(sin_t) < eps:
+        #             sin_t = eps if sin_t >= 0 else -eps
+
+        #         r = a / sin_t - l # polar version of conchoid
+        #         return np.array([r * np.cos(t), r * np.sin(t), 0]) # converting coords into cartesian
+
+        #     large_conchoid = always_redraw(lambda: ParametricFunction(
+        #         conchoid_func,
+        #         t_range=[0, tMax.get_value()],
+        #         color=YELLOW
+        #     ))
+
+        #     small_conchoid = always_redraw(lambda: ParametricFunction(
+        #         conchoid_func_minus,
+        #         t_range=[0, tMax.get_value()],
+        #         color=YELLOW
+        #     ))
+
+        #     self.add(large_conchoid, small_conchoid)  
+        #     curves.add(large_conchoid)
+        #     curves.add(small_conchoid)
+        #     self.play(tMax.animate.set_value(PI), run_time=5, rate_func=linear)
+        #     self.wait(80)
+
+        #     self.play(FadeOut(lvalue))
+
+        # for i in range(5):
+        #     ConchoidConstruct(-i)
+
+        # self.wait(3)
+
+        # ConchoidOfNicomedseEnd = Tex("Conchoid of Nicomedes")
+        # ConchoidOfNicomedseEnd.font_size = 90
+        # self.play(FadeIn(ConchoidOfNicomedseEnd), FadeOut(curves))
+        # self.wait()
